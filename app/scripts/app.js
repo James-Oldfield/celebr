d869c7a353f8b3ca8c8c8d8275ec68bcf1a351de
James-Oldfield <hi@james-oldfield.co>
2015-03-06 14:59:46 +0000

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ryanApp
+ * @description
+ * # ryanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ryanApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ryanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ryanApp
+ */
+angular.module('ryanApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
